1f3bae76dd984e597a66ecdab865f771eb986af3
Antediluvianist <alexrob260@gmail.com>
2025-01-25 16:40:25 +0100

    ajout html/css, début de la récupération du DOM via JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-lancerJeu()
+//lancerJeu()
+
+let inputEcriture = document.getElementById("inputEcriture")
+let btnValidaterMot = document.getElementById("btnValiderMot")
+
+let zoneProposition = document.querySelector(".zoneProposition")
+let spanScore = document.querySelector(".zoneScore span")
+
+let listeBtnRadio = document.querySelectorAll(".optionSource input")
+
